b16af0fe68541f1d5e56486843a63216590bf57a
JoyceM137 <JoyceM137@a941861c-d1c0-11de-aacf-132551bdafba>
2009-11-15 10:01:23 +0000

    Added basic game board display code.  Fixes Issue 1

---
+++ src/gamegalaxy/arimaa/data/SpaceData.java
@@ -0,0 +1,55 @@
+/* 
+ *  LEGAL STUFF
+ * 
+ *  This file is part of gamegalaxy.
+ *  
+ *  gamegalaxy is Copyright 2009 Joyce Murton
+ *  
+ *  Arimaa and other content here copyright their respective copyright holders.
+ *  
+ *  gamegalaxy is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *   
+ *  gamegalaxy is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details
+ *   
+ *  You should have received a copy of the GNU General Public License
+ *  along with gamegalaxy.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package gamegalaxy.arimaa.data;
+
+/**
+ * 
+ */
+public class SpaceData
+{
+
+	public static final int NORMAL = 0;
+	public static final int TRAP = 1;
+	private int	spaceType;
+	
+	/**
+	 * TODO: Describe constructor
+	 *
+	 * @param trap2
+	 */
+	public SpaceData(int spaceType)
+	{
+		this.spaceType = spaceType;
+	}
+
+	/**
+	 * TODO: Describe method
+	 *
+	 * @return
+	 */
+	public int getSpaceType()
+	{
+		return spaceType;
+	}
+
+}
